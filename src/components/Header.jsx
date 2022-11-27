64da389f574409c3a4c09ae7a2d5e7cdcfa5f2a8
dev-bakery <rimming2255@naver.com>
2022-11-27 21:40:45 +0900

    로그인

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Header() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    onUserStateChange((user) => setUser(user));
+  }, []);
+
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -13,11 +20,23 @@ export default function Header() {
 
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>products</Link>
-        <Link to='/products/new'>
-          <BsFillPencilFill />
-        </Link>
+        {user && user.isAdmin && (
+          <Link to='/products/new'>
+            <BsFillPencilFill />
+          </Link>
+        )}
         <Link to='/cart'>cart</Link>
-        <button type='button'>Login</button>
+        {!user && (
+          <button type='button' onClick={login}>
+            Login
+          </button>
+        )}
+        {user && <User user={user} />}
+        {user && (
+          <button type='button' onClick={logout}>
+            Logout
+          </button>
+        )}
       </nav>
     </header>
   );
